422ee2c392ca8411bbd33c12172673572907ba6c
sshonosuke <50283114+sshonosuke@users.noreply.github.com>
2020-06-17 16:53:04 +0900

    Add files via upload

--- Example.R
+++ Example.R
@@ -1,66 +1,36 @@
-source("CCR-function-new.R")
+source("CCR-BMA-function.R")
 library(mada)
 
 # Data
 data(AuditC)
-dd=madad(AuditC)
+dd <- madad(AuditC)
 
-logit=function(x){ log(x/(1-x)) }
-invlogit=function(x){ exp(x)/(1+exp(x)) }
+logit <- function(x){ log(x/(1-x)) }
+invlogit <- function(x){ exp(x)/(1+exp(x)) }
 
-pp=cbind(dd$sens$sens,1-dd$spec$spec)
-y=logit(pp)
-ni=cbind(AuditC$TP+AuditC$FN,AuditC$FP+AuditC$TN)
-S=1/(pp*(1-pp)*ni)
-
-
-# Proposed confidence region
-fit=CCR(y,S,alpha=0.05)
-
-h.ast=fit[[2]]
-hbeta=fit[[3]]
-IH=fit[[4]]
-xx=fit[[5]]
-
-Theta=seq(0,2*pi,length=200)
-Lam=eigen(IH)$values
-HH=t(eigen(IH)$vectors)
-t(HH)%*%diag(Lam)%*%HH
-
-c=xx*(1+h.ast)
-tx=sqrt(c/Lam)*rbind(cos(Theta),sin(Theta))
-CCR=t(hbeta+t(HH)%*%tx)
+pp <- cbind(dd$sens$sens, 1-dd$spec$spec)
+Y <- logit(pp)
+ni <- cbind(AuditC$TP+AuditC$FN, AuditC$FP+AuditC$TN)
+S <- 1/(pp*(1-pp)*ni)
+n <- dim(Y)[1]
 
 
+# Confidence region
+CR <- CCR.BMA(Y, S, alpha=0.05)
 
 # Reitsuma's method
-fits=reitsma(AuditC,method="reml")
+fits <- reitsma(AuditC, method="reml")
 
-QQ <- apply(diag(fits$Psi)*t(1/S), 1, sum)
-m <- dim(ni)[1]
-(QQ-(m-1))/QQ*100
+# heterogeneity measure
+ww <- 1/S
+QQ <- (n-1)*apply(ww, 2, sum) / (apply(ww, 2, sum)^2 - apply(ww^2, 2, sum))
+hPsi <- diag(fits$Psi)
+hPsi/(QQ+hPsi)*100
 
-# load the result of Sugasawa and Noma (2019)
-load("AuditC-result.RData")
 
 # Plot
-postscript("AuditC.eps",height=8,width=8,pointsize=16,horizontal=F)
-plot(fits,sroclwd=2,main ="",
-     ylim=c(0.5,1),xlim=c(0,0.52))
-points(invlogit(CCR)[,c(2,1)],type="l",lty=2)
-points(invlogit(sECR)[,c(2,1)],type="l",lty=3)
-points(invlogit(y)[,c(2,1)],pch=2)
-legend("bottomright",c("Data point","Summary estimate","SROC","Approximate CR","Corrected CR","Exact CR"),
-       pch=c(2,1,NA,NA,NA,NA),lwd=c(NA,NA,2,1,1,1),lty=c(NA,NA,1,1,2,3))
-dev.off()
-
-
-pdf("AuditC.pdf",height=8,width=8,pointsize=16)
-plot(fits,sroclwd=2,main ="",
-     ylim=c(0.5,1),xlim=c(0,0.52))
-points(invlogit(CCR)[,c(2,1)],type="l",lty=2)
-points(invlogit(sECR)[,c(2,1)],type="l",lty=3)
-points(invlogit(y)[,c(2,1)],pch=2)
-legend("bottomright",c("Data point","Summary estimate","SROC","Approximate CR","Corrected CR","Exact CR"),
-       pch=c(2,1,NA,NA,NA,NA),lwd=c(NA,NA,2,1,1,1),lty=c(NA,NA,1,1,2,3))
-dev.off()
+plot(fits, sroclwd=2, ylim=c(0.5,1), xlim=c(0,0.52))
+points(invlogit(CR)[,c(2,1)], type="l", lty=2)
+points(invlogit(Y)[,c(2,1)], pch=2)
+meth <- c("Data point","Summary estimate","SROC","Approximate CR","Corrected CR")
+legend("bottomright", legend=meth, pch=c(2,1,NA,NA,NA), lwd=c(NA,NA,2,1,1), lty=c(NA,NA,1,1,2))
